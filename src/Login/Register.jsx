2fca2dfb45cc16e5d86997dfcea52493e307f8ef
pinkir <pinki.roy@outlook.com>
2023-06-08 00:01:18 +0600

    login

--- src/Login/Register.jsx
+++ src/Login/Register.jsx
@@ -1,8 +1,53 @@
-
+import bg from '../images/bg2.jpg'
 const Register = () => {
     return (
-        <div>
-            
+        <div className="hero min-h-screen" style={{backgroundImage: `url(${bg})`}}>
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-sky-600 bg-opacity-20 my-8">
+                    <div className="card-body">
+                        <div className="form-control">
+                        <h1 className="text-4xl font-bold">Please Sign in!</h1>
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="name" 
+                            name="name"
+                            className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                        
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email"
+                            name="email"
+                             className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" 
+                            name="password"
+                            className="input input-bordered" />
+                            
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Confirm Password</span>
+                            </label>
+                            <input type="confirm" placeholder="confirm password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-info">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import './index.css'
 import router from './Routes/Route.jsx'
 
 import {
   RouterProvider,
 } from "react-router-dom";
+import AuthProvider from './Providers/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <div className='max-w-7xl mx-auto'>
-     <RouterProvider router={router} />
-     </div>
+    <AuthProvider>
+      <div className='max-w-7xl mx-auto'>
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>,
 )
